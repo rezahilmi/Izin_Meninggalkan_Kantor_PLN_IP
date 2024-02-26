394356af9294d11d4f547db4101ea84275710fe8
Reza Hilmi Dafa <100016490+RezaHD0@users.noreply.github.com>
2024-02-26 11:51:31 +0700

    full commit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+
+            session(['redirect' => url()->previous()]);
+            
+            return route('login');
+        }
+        // return $request->expectsJson() ? null : route('login');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nip',
         'email',
         'password',
+        'role',
+        'temporary_role',
     ];
 
     /**
